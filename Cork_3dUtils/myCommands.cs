32c2e0af20e18fc9c8c04ae694f54c89082b0b76
Zach Ayers <ayers.dev@icloud.com>
2018-04-04 20:37:43 -0500

    Anno Util Class Added

--- Cork_3dUtils/myCommands.cs
+++ Cork_3dUtils/myCommands.cs
@@ -1,36 +1,29 @@
-﻿// (C) Copyright 2018 by  
-//
-using System;
-using Autodesk.AutoCAD.Runtime;
-using Autodesk.AutoCAD.ApplicationServices;
+﻿// (C) Copyright 2018 by Zach Ayers
+// CORK plugin for AutoCAD
+// Email: zachayers@users.noreply.github.com
+
+using Autodesk.AutoCAD.ApplicationServices.Core;
 using Autodesk.AutoCAD.DatabaseServices;
-using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Cork_3dUtils;
 
 // This line is not mandatory, but improves loading performances
-[assembly: CommandClass(typeof(Cork_3dUtils.MyCommands))]
+
+[assembly: CommandClass(typeof (MyCommands))]
 
 namespace Cork_3dUtils
 {
-
     // This class is instantiated by AutoCAD for each document when
     // a command is called by the user the first time in the context
     // of a given document. In other words, non static data in this class
     // is implicitly per-document!
     public class MyCommands
     {
-        // The CommandMethod attribute can be applied to any public  member 
-        // function of any public class.
-        // The function should take no arguments and return nothing.
-        // If the method is an intance member then the enclosing class is 
-        // intantiated for each document. If the member is a static member then
-        // the enclosing class is NOT intantiated.
-        //
-        // NOTE: CommandMethod has overloads where you can provide helpid and
-        // context menu.
-
-        // Modal Command with localized name
-        [CommandMethod("FloorMany", CommandFlags.Modal)]
+        // Floor Command
+        // Prompts user to select objects, then select a point to align with current Z Plane
+        [CommandMethod("Floor", CommandFlags.Modal)]
         public static void CmdFloorMany() // This method can have any name
         {
             // Initialize Document
@@ -38,20 +31,19 @@ namespace Cork_3dUtils
             var acCurDb = acDoc.Database;
             var acCurEd = acDoc.Editor;
 
-            // Check if current document exists
-            if (acDoc == null) return;
-
             // Open a Transaction
             // Start a transaction
-            using (Transaction acTrans = acCurDb.TransactionManager.StartTransaction())
+            using (var acTrans = acCurDb.TransactionManager.StartTransaction())
             {
                 // Set selection options for user
-                var selOpt = new PromptSelectionOptions();
-                selOpt.MessageForAdding = "\nSelect objects to move to Z Plane: ";
-                selOpt.MessageForRemoval = "\nSelect objects to remove from selection: ";
-                selOpt.RejectPaperspaceViewport = true;
-                selOpt.RejectObjectsOnLockedLayers = true;
-                selOpt.RejectObjectsFromNonCurrentSpace = true;
+                var selOpt = new PromptSelectionOptions
+                {
+                    MessageForAdding = "\nSelect objects to move to Z Plane: ",
+                    MessageForRemoval = "\nSelect objects to remove from selection: ",
+                    RejectPaperspaceViewport = true,
+                    RejectObjectsOnLockedLayers = true,
+                    RejectObjectsFromNonCurrentSpace = true
+                };
 
                 // Get object selection from user
                 var selRes = acCurEd.GetSelection(selOpt);
@@ -63,48 +55,36 @@ namespace Cork_3dUtils
                 var acSSet = selRes.Value;
 
                 // Get point from user to floor
-                var ptOpt = new PromptPointOptions("Select point to floor: ");
-                ptOpt.AllowNone = true;
+                var ptOpt = new PromptPointOptions("Select Vertex to align with Z Plane: ") {AllowNone = false};
                 var ptRes = acCurEd.GetPoint(ptOpt);
 
                 // Exit if selection error
                 if (ptRes.Status != PromptStatus.OK) return;
 
                 // Create  matrix to move the objects from the current Z value to a '0' Z value
-                var acPt3d = ptRes.Value;
-                var floorPt3D = new Point3d(acPt3d.X, acPt3d.Y, 0);
-                var acVec3d = acPt3d.GetVectorTo(floorPt3D);
-                var transVec3D = acVec3d.TransformBy(acCurEd.CurrentUserCoordinateSystem);
-
-                acCurEd.WriteMessage($"\nSelected Point: {acPt3d.X}, {acPt3d.Y}, {acPt3d.Z}");
-                acCurEd.WriteMessage($"\nFloor Point: {floorPt3D.X}, {floorPt3D.Y}, {floorPt3D.Z}");
-                acCurEd.WriteMessage($"\nDisplacement Vector: {acVec3d.X}, {acVec3d.Y}, {acVec3d.Z}");
-                acCurEd.WriteMessage($"\nTransformed Vector: {transVec3D.X}, {transVec3D.Y}, {transVec3D.Z}");
+                var acPt3D = ptRes.Value;
+                var floorPt3D = new Point3d(acPt3D.X, acPt3D.Y, 0);
+                var acVec3D = acPt3D.GetVectorTo(floorPt3D);
+                var transVec3D = acVec3D.TransformBy(acCurEd.CurrentUserCoordinateSystem);
 
                 // Floor selected objects
-                foreach (SelectedObject acSSObj in acSSet)
+                foreach (SelectedObject acSsObj in acSSet)
                 {
                     // Check if a valid SelectedObject was returned
-                    if (acSSObj != null)
-                    {                       
+                    if (acSsObj != null)
+                    {
                         // Open the selected object for write
-                        var acEnt = acTrans.GetObject(acSSObj.ObjectId, OpenMode.ForWrite) as Entity;
+                        var acEnt = acTrans.GetObject(acSsObj.ObjectId, OpenMode.ForWrite) as Entity;
 
-                        if (acEnt != null)
-                        {                          
-                            acEnt.TransformBy(Matrix3d.Displacement(transVec3D));
-                        }
+                        acEnt?.TransformBy(Matrix3d.Displacement(transVec3D));
                     }
                 }
 
-                acCurEd.WriteMessage("\nSelection has been floored");
+                acCurEd.WriteMessage("\nSelection has been aligned with Z Plane.");
 
                 // Dispose the transaction
                 acTrans.Commit();
             }
-
-        }          
-
+        }
     }
-
 }
---
+++ Cork_AnnoUtils/myCommands.cs
@@ -0,0 +1,26 @@
+﻿// (C) Copyright 2018 by Zach Ayers
+// CORK plugin for AutoCAD
+// Email: zachayers@users.noreply.github.com
+
+using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(Cork_AnnoUtils.MyCommands))]
+
+namespace Cork_AnnoUtils
+{
+
+
+    public class MyCommands
+    {
+        
+        //Todo
+
+    }
+
+}
